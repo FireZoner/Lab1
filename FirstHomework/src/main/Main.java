670b6cf56d89b7b6f38b2b253ede04f4bfcc8668
Pavel <zubbodazdeban@mail.ru>
2026-03-02 01:13:47 +0300

    one more another change

--- FirstHomework/src/main/Main.java
+++ FirstHomework/src/main/Main.java
@@ -16,7 +16,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.print("Hello ");
-        System.out.println("World too!");
+        System.out.println("World too from me!");
     }
     
 }
